d13de5f500f585a15802b60dd65a1e160850ed8b
miguel <miguel.amoedo.cardoso@gmail.com>
2017-08-01 17:29:13 -0300

    Figurinhas (mdc)

---
+++ mdc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (){
+
+	int N, R, V, i, resto;
+
+	scanf("%d", &N);
+	int mdc[N];
+
+	for(i = 0; i < N; i++)
+	{
+		scanf("%d%d", &R, &V);
+
+		resto=R%V;
+        while(resto!=0)
+        {
+	        R = V;
+	        V = resto;
+	        resto = R%V;         
+        }
+
+        mdc[i]=V;
+	}
+
+	for(i = 0; i < N; i++)
+	{
+		printf("%d\n", mdc[i]);
+	}
+	return 0;
+}
