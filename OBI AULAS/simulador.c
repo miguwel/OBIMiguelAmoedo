93c6c486c9a9a0ef9fea2420688c1d83b4eb973b
CactoLeproso <rodjucateixeira@gmail.com>
2017-12-12 14:57:00 -0200

    simulador.c

--- OBI AULAS/simulador.c
+++ OBI AULAS/simulador.c
@@ -10,47 +10,45 @@ int soma(int X, int Y){
 	while(i <= Y)
 	{
 		s = s + val[i];
+		i++;
 	}
 	return s;
 }
 
 void inverter(int X, int Y){
-	int x, y;
+	int x;
 	int i = 0;
 	while(X+i <= Y-i)
 	{
 		x = val[X+i];
-		y = val[Y-i];
-		val[X+i] = y;
+		val[X+i] = val[Y-i];
 		val[Y-i] = x;
+		i++;
 	}
 }
 
 int main(){
 	char op;
 	int i, Npos, Nop, X, Y;
-	scanf("%d%d\n", &Npos, &Nop);
+	
+	scanf("%d %d", &Npos, &Nop);
 	printf("%d %d\n", Npos, Nop);
 	for(i = 1; i <= Npos; i++)
 	{
 		val[i] = i;
 	}
-	scanf("%c%d%d", &op, &X, &Y);
-	printf("%c %d %d\n", op, X, Y);
-
-	/*for(i = 0; i < Nop; i++)
+	
+	for(i = 0; i < Nop; i++)
 	{
-		
+		scanf("%c %d %d", &op, &X, &Y);
+		printf("%c\n", op);
 
 		if(op == 'S')
 			printf("%d\n", soma(X,Y));
 
 		else if(op == 'I')
 			inverter(X,Y);
-
-		else
-			printf("Operacao Invalida\n");
-	}*/
+	}
 
 	return 0;
 }
